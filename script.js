33611a7923f97097df118c84a48ed8124412a60e
cintiaalfaro29 <alfarocintiamarina@gmail.com>
2025-06-04 17:15:32 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//------------------------MINI ENTREGA3---------------------
+
+let nombre = prompt("Bienvenido ¿cuàl es tu nombre?");
+let mascota = prompt("¿y el de tu mascota?");
+
+//mostrar al finalizar el formulario
+function postFormulario(nombre, mascota) {
+    alert(`Hola ${nombre}, gracias por confiar en nosotros para trasladar a ${mascota}`);
+}
+postFormulario(nombre,mascota);
+
